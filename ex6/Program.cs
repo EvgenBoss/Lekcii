03734fc71405ef3927fdeb7408575ee1674ece0a
Evgen <Francesbean1999166@mail.ru>
2022-06-30 22:14:27 +0300

    max гиря

---
+++ ex6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 5;
+int c = 7;
+int d = 123;
+int e = 6;
+int f = 90;
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+ 
+ Console.Write ("Max = ");
+ Console.WriteLine(max);
