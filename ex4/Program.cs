3af5b77493fedc1235b5e233d628aa85aa55386b
Evgen <Francesbean1999166@mail.ru>
2022-06-30 22:00:27 +0300

    Деление чисел не целое

--- ex4/Program.cs
+++ ex4/Program.cs
@@ -1,4 +1,4 @@
-﻿int numberA = 20;
-int numberB = 10;
-int numberC = numberA / numberB;
+﻿Double numberA = 2012312;
+Double numberB = 10342;
+Double numberC = numberA / numberB;
 Console.WriteLine(numberC);
