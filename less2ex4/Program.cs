e0c430b356c2393decf56ae49b0c23f71749cf37
Evgen <Francesbean1999166@mail.ru>
2022-07-14 12:59:20 +0300

    методы со случ числами

--- less2ex4/Program.cs
+++ less2ex4/Program.cs
@@ -12,15 +12,34 @@
 void PrintArray(int[] call)
 {
     int count = call.Length;
-    int pos = 0;
-    while(pos < count)
+    int position = 0;
+    while(position < count)
     {
-        Console.WriteLine(call[pos]);
-        pos++;
+        Console.WriteLine(call[position]);
+        position++;
     }
+}
+
+int indexOF(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position =0;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
 }
 
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+int pos = indexOF(array, 4);
+Console.WriteLine(pos);
