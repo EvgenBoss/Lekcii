63b22cb2c700f6c4e34e444e5035c35cf4140f8d
Evgen <Francesbean1999166@mail.ru>
2022-07-14 12:43:35 +0300

    методы

---
+++ less2ex4/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random() .Next(1, 10); 
+        index++;
+    }
+}
+
+void PrintArray(int[] call)
+{
+    int count = call.Length;
+    int pos = 0;
+    while(pos < count)
+    {
+        Console.WriteLine(call[pos]);
+        pos++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
