872ce168e56c7db0a861d000577576f090f15b15
Evgen <Francesbean1999166@mail.ru>
2022-07-19 10:30:07 +0300

    упорядочивание методом минимума

---
+++ Less3Sort(min)/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] array = {1,7,9,4,6,3,5,2,8,6}; 
+void printarray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0;i < length; i++ )
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void select(int[] arr)
+{
+    for (int i = 0;i < arr.Length - 1; i++ )
+    {
+        int min = i;
+        for(int j = i + 1;j < arr.Length; j++ )
+        {
+            if(arr[j] < array[min]) min = j;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+}
+
+printarray(array);
+select(array);
+
+printarray(array);
