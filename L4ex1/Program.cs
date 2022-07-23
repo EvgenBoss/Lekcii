e9014142a4f47aa77ba6a6c6a6dfca31ff3c8381
Evgen <Francesbean1999166@mail.ru>
2022-07-23 22:28:29 +0300

    двумерный массив

---
+++ L4ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿void PrintArray(int[,] mat)
+{
+for(int i = 0; i < mat.GetLength(0); i++)
+{
+    for(int j = 0; j < mat.GetLength(1); j++)
+    {
+        Console.Write($"{mat[i,j]}");
+    }
+    Console.WriteLine();
+}
+}
+void FillArray(int[,] mat)
+{
+    for(int i = 0; i < mat.GetLength(0); i++)
+{
+    for(int j = 0; j < mat.GetLength(1); j++)
+    {
+        mat[i,j] = new Random().Next(1,10);
+    }
+    Console.WriteLine();
+}
+}
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
